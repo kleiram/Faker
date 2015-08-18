414e1e7b4a83d05f272b859ee4635fa6c2e839cd
Ramon Kleiss <ramonkleiss@gmail.com>
2015-08-18 14:09:38 +0200

    Create the Company generator

---
+++ Source/Classes/Company.swift
@@ -0,0 +1,71 @@
+// Company.swift
+//
+// Copyright (c) 2014–2015 Apostle (http://apostle.nl)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+public class Company {
+    // MARK: Provider
+    
+    public class Provider {
+        public init() {
+            // noop
+        }
+        
+        func formats() -> [String] {
+            return [
+                "\(Person.lastName()) \(Company.companySuffix())"
+            ]
+        }
+        
+        func suffixes() -> [String] {
+            return [ "Ltd." ]
+        }
+    }
+    
+    // MARK: Variables
+    
+    public static var provider : Provider?
+    
+    // MARK: Generators
+    
+    /**
+        Generate a random company name.
+
+        - returns: Returns a random company name.
+    */
+    public class func company() -> String {
+        return dataProvider().formats().random()!
+    }
+    
+    /**
+        Generate a random company suffix.
+
+        - returns: Returns a random company suffix.
+    */
+    public class func companySuffix() -> String {
+        return dataProvider().suffixes().random()!
+    }
+    
+    private class func dataProvider() -> Provider {
+        return provider ?? Provider()
+    }
+}
