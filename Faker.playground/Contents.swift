1f79e5cf0ca13bb55a924e38f3ffa3a551fb75ca
Ramon Kleiss <ramonkleiss@gmail.com>
2015-08-18 14:09:03 +0200

    Clear the playground

--- Faker.playground/Contents.swift
+++ Faker.playground/Contents.swift
@@ -1,8 +1,3 @@
 //: Playground - noun: a place where people can play
 
 import Faker
-
-let avatar    = UIImage.avatar()
-let imageView = UIImageView(image: avatar)
-imageView.layer.cornerRadius = (avatar?.size.width)! / 4
-imageView.clipsToBounds      = true
